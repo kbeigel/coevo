1ee6bce24d90c4c43d094b935395b449bc301e6c
kat beigel <47261995+kbeigel@users.noreply.github.com>
2024-12-02 17:07:23 -0500

    Create ingest_data.R

---
+++ R/ingest_data.R
@@ -0,0 +1,51 @@
+# Functions for ingesting phylogenetic trees and cleaning up names
+
+# Read in an association matrix
+read_associations = function(filepath) {
+    # Read in the host-symbiont association matrix)
+    assoc_mat = as.matrix(
+        read.table(
+            filepath,
+            header = TRUE,
+            check.names = FALSE
+        )
+    )
+
+    assoc_info = list(
+        assoc_matrix = assoc_mat,
+        host_taxa = rownames(assoc_mat),
+        symbiont_taxa = colnames(assoc_mat)
+    )
+
+    return(assoc_info)
+}
+
+# Read in phylogenetic tree from a .nexus file
+read_phylo = function(filepath, nexus_format, taxa_to_keep=NULL, label_fix=NULL) {
+
+    # read in the nexus tree
+    message("Reading nexus file.")
+    phylo = phytools::readNexus(
+        filepath,
+        nexus_format
+    )
+
+    if (!is.null(label_fix)) {
+        message(paste0("Replacing '", label_fix[1], "' in taxa names with '", label_fix[2], "'"))
+        phylo[['tip.label']] = stringr::str_replace(
+            phylo[['tip.label']],
+            label_fix[1],
+            label_fix[2]
+        )
+    }
+
+    if (!is.null(taxa_to_keep)) {
+        message(paste0("Keeping only taxa in provided list: ", paste0(taxa_to_keep, collapse = ', ')))
+        phylo = ape::keep.tip(
+            phylo,
+            taxa_to_keep
+        )
+    }
+
+    return(phylo)
+}
